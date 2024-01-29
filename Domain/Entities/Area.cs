3dd2d1143ec2729cc5c18b774ead59dc441c7972
ToanNguyen9602 <toannguyen9602@gmail.com>
2024-01-29 16:16:40 +0700

    commit Area Class

--- Domain/Entities/Area.cs
+++ Domain/Entities/Area.cs
@@ -11,6 +11,13 @@ namespace Domain.Entities
     [Table("Area")]
     public class Area
     {
-      
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Area_code { get; set; }
+
+        [Column("AreaName", TypeName = "nvarchar(50)")]
+        public string AreaName { get; set; }
     }
 }
