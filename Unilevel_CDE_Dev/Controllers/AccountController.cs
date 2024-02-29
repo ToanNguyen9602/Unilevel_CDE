21929295b796231ec2e2c87cb4a10f8d844c21d8
ToanNguyen9602 <toannguyen9602@gmail.com>
2024-02-29 18:08:34 +0700

    commit Account_Controller

---
+++ Unilevel_CDE_Dev/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unilevel_CDE_Dev.Controllers
+{
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        private AccountService AccountService;
+        private IWebHostEnvironment webHostEnvironment;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] Account account)
+        {
+            try
+            {
+                account.Password = BCrypt.Net.BCrypt.HashPassword(account.Password);
+                return Ok(new
+                {
+                    status = AccountService.Create(account)
+                });
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [Produces("application/json")]
+        [HttpGet("login/{username}&{password}")]
+        public IActionResult FindByCreated(string username, string password)
+        {
+            try
+            {
+                return Ok(new
+                {
+                    status = AccountService.Login(username, password)
+                });
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] Account account)
+        {
+            try
+            {
+                account.Password = BCrypt.Net.BCrypt.HashPassword(account.Password);
+                return Ok(new
+                {
+                    status = AccountService.Update(account)
+                });
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
--- Unilevel_CDE_Dev/Services/AccountServiceImpl.cs
+++ Unilevel_CDE_Dev/Services/AccountServiceImpl.cs
@@ -4,9 +4,24 @@ namespace Unilevel_CDE_Dev.Services
 {
     public class AccountServiceImpl : AccountService
     {
+        private DatabaseContext db;
+
+        public AccountServiceImpl(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
         public bool Create(Account Account)
         {
-            throw new NotImplementedException();
+            try
+            {
+                db.Accounts.Add(account);
+                return db.SaveChanges() > 0;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public dynamic FindId(int id)
@@ -16,12 +31,26 @@ namespace Unilevel_CDE_Dev.Services
 
         public bool Login(string username, string password)
         {
-            throw new NotImplementedException();
+            var account = db.Accounts.SingleOrDefault(a => a.Username == username && a.Status);
+            if (account != null)
+            {
+                return BCrypt.Net.BCrypt.Verify(password, account.Password);
+            }
+            return false;
         }
 
         public bool Update(Account account)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                db.Entry(account).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                return db.SaveChanges() > 0;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
